db3960021fd98e5a4115ca08b06ec50b2754eb41
Fredrik Wikstrom <fredrik@a500.org>
2018-02-06 15:37:41 +0200

    Bumped version to 53.8.

--- zip.library_rev.h
+++ zip.library_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	7
-#define DATE		"13.1.2018"
-#define VERS		"zip.library 53.7"
-#define VSTRING		"zip.library 53.7 (13.1.2018)\r\n"
-#define VERSTAG		"\0$VER: zip.library 53.7 (13.1.2018)"
+#define REVISION	8
+#define DATE		"6.2.2018"
+#define VERS		"zip.library 53.8"
+#define VSTRING		"zip.library 53.8 (6.2.2018)\r\n"
+#define VERSTAG		"\0$VER: zip.library 53.8 (6.2.2018)"
