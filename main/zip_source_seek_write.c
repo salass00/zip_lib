18f5d88b6b1dda6ddfa646c0921fbd74e862ea6e
Fredrik Wikstrom <fredrik@a500.org>
2018-04-13 12:58:34 +0300

    Updated copyright years.

--- main/zip_source_seek_write.c
+++ main/zip_source_seek_write.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2017 Fredrik Wikstrom <fredrik@a500.org>
+ * Copyright (C) 2014-2018 Fredrik Wikstrom <fredrik@a500.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
