408bdc64e7015c5225fdb2df7a76087132f010e4
Fredrik Wikstrom <fredrik@a500.org>
2021-01-31 09:00:54 +0200

    Bumped version to 53.12.

--- zip.library_rev.h
+++ zip.library_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	11
-#define DATE		"30.1.2021"
-#define VERS		"zip.library 53.11"
-#define VSTRING		"zip.library 53.11 (30.1.2021)\r\n"
-#define VERSTAG		"\0$VER: zip.library 53.11 (30.1.2021)"
+#define REVISION	12
+#define DATE		"31.1.2021"
+#define VERS		"zip.library 53.12"
+#define VSTRING		"zip.library 53.12 (31.1.2021)\r\n"
+#define VERSTAG		"\0$VER: zip.library 53.12 (31.1.2021)"
